9800bc3d077657da1cd806aa74e2ce5638a9d49c
Fabian Mahdi <fabian@mahdi.at>
2024-05-18 17:49:29 +0200

    added a few example pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css';
 import Footer from './Components/Footer/Footer';
 import Navigationbar from './Components/Navigationbar/Navigationbar';
+import Register from './Components/Register/Register';
 
 function App() 
 {
     return (
         <>
             <Navigationbar/>
+            <div className='main'>
+                <Register />
+            </div>
             <Footer/>
         </>
     );

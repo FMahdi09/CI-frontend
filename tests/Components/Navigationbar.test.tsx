87c72cd6f1c511113a7d7548381a8d552394e0fb
Fabian Mahdi <fabian@mahdi.at>
2024-05-18 20:25:25 +0200

    added user event testing library and additional tests for navigationbar

--- tests/Components/Navigationbar.test.tsx
+++ tests/Components/Navigationbar.test.tsx
@@ -1,15 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import Navigationbar from '../../src/Components/Navigationbar/Navigationbar';
 import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 
 describe('Navigationbar', () => 
 {
     const renderComponent = () =>
     {
         render(
-            <MemoryRouter>
-                <Navigationbar/>
-            </MemoryRouter>
+            <Navigationbar/>,
+            {wrapper: MemoryRouter}
         );
 
         return {
@@ -17,11 +17,21 @@ describe('Navigationbar', () =>
             helpLink:     screen.getByRole('link', {name:/help/i}),
             loginLink:    screen.getByRole('link', {name:/login/i}),
             registerLink: screen.getByRole('link', {name:/register/i}),
+            user: userEvent.setup(),
         };
     };
 
     it('should render all elements without failure', () => 
     {
         renderComponent();
+    });
+
+    it('should mark the explore button as active after clicking on it', async () => 
+    {
+        const {exploreLink, user} = renderComponent();
+        
+        await user.click(exploreLink);
+
+        expect(exploreLink.className).toMatch(/active/i);
     });
 });

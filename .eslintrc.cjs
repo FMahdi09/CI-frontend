2fddbfe71a1421de5a85fcc94afa0bc29800ee7c
Fabian Mahdi <fabian@mahdi.at>
2024-05-18 09:40:00 +0200

    updated linting rules to be type aware

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,11 +3,22 @@ module.exports = {
     env: { browser: true, es2020: true },
     extends: [
         'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/strict-type-checked',
         'plugin:react-hooks/recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime'
     ],
-    ignorePatterns: ['dist', '.eslintrc.cjs'],
-    parser: '@typescript-eslint/parser',
+    ignorePatterns: ['dist', '.eslintrc.cjs', 'vitest.config.ts'],
+    parserOptions: {
+        sourceType: 'module',
+        project: ['./tsconfig.json', './tsconfig.node.json'],
+        parser: '@typescript-eslint/parser',
+    },
+    "settings": {
+        "react": {
+            "version": "detect"
+        }
+    },
     plugins: ['react-refresh'],
     rules: {
         'indent': ['error', 4],

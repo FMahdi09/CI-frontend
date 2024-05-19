5c50ad01a318db485b4cd506e325f391d3d980e8
Fabian Mahdi <fabian@mahdi.at>
2024-05-19 11:39:17 +0200

    added sample Welcomepage + routing for default route

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Register from './Components/Register/Register';
 import Explore from './Components/Explore/Explore';
 import Help from './Components/Help/Help';
 import Login from './Components/Login/Login';
+import Welcome from './Components/Welcome/Welcome';
 
 function App() 
 {
@@ -14,11 +15,12 @@ function App()
             <Navigationbar/>
             <div className='main primary-background'>
                 <Routes>
-                    <Route path='/' element={<Explore/>}/>
+                    <Route path='/' element={<Welcome/>}/>
                     <Route path='/explore/*' element={<Explore/>}/>
                     <Route path='/help' element={<Help/>}/>
                     <Route path='/register' element={<Register/>}/>
                     <Route path='/login' element={<Login/>}/>
+                    <Route path='*' element={<Welcome/>}/>
                 </Routes>
             </div>
             <Footer/>

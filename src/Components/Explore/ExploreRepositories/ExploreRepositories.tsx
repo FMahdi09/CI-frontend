bd736751fd268f48b5df49dcb9a712530a25852e
Fabian Mahdi <fabian@mahdi.at>
2024-05-19 21:30:38 +0200

    added margin to repo searchbar

--- src/Components/Explore/ExploreRepositories/ExploreRepositories.tsx
+++ src/Components/Explore/ExploreRepositories/ExploreRepositories.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import Searchbar from '../../Searchbar/Searchbar';
+import './ExploreRepositories.css';
 
 function ExploreRepositories()
 {
     const [query, setQuery] = useState('');
 
     return (
-        <Searchbar placeholder='Search repos...' searchCallback={setQuery}>
+        <Searchbar className='searchbar' placeholder='Search repos...' searchCallback={setQuery}>
             Search
         </Searchbar>
     );

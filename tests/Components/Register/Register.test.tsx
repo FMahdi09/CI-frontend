e23fa69fb68ac4d10f08f72a986f26076b1b752a
Fabian Mahdi <fabian@mahdi.at>
2024-05-20 16:21:30 +0200

    fixed unit test for Register component

--- tests/Components/Register/Register.test.tsx
+++ tests/Components/Register/Register.test.tsx
@@ -1,11 +1,16 @@
 import { render } from '@testing-library/react';
 import Register from '../../../src/Components/Register/Register';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('Register', () => 
 {
     const renderComponent = () => 
     {
-        render(<Register />);
+        render(
+            <MemoryRouter>
+                <Register />
+            </MemoryRouter>
+        );
     };
 
     it('should render all elements without failure', () => 

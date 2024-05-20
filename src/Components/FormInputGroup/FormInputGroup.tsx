95b9350f59c15adf4ee0f4095c8d8e02ac748332
Fabian Mahdi <fabian@mahdi.at>
2024-05-20 22:47:32 +0200

    added register form

---
+++ src/Components/FormInputGroup/FormInputGroup.tsx
@@ -0,0 +1,34 @@
+import { RefObject } from 'react';
+
+interface props
+{
+    description: string,
+    hasError: boolean,
+    inputRef?: RefObject<HTMLInputElement>,
+    type?: string,
+    onChange(value: string) : void
+}
+
+function FormInputGroup({description, hasError, inputRef, type, onChange}: props)
+{
+    return (
+        <div className='form-element'>
+            <label htmlFor={description} className={'form-label ' + (hasError ? 'error-text' : '')}>
+                {description}
+            </label>
+            <input 
+                type={type || 'string'}
+                id={description}
+                className={'form-input ' + (hasError ? 'error-border error-background' : '')}
+                onChange={(e) => 
+                {
+                    onChange(e.target.value);
+                }}
+                required
+                ref={inputRef}
+            />
+        </div>
+    );
+}
+
+export default FormInputGroup;
--- src/Components/Register/Register.tsx
+++ src/Components/Register/Register.tsx
@@ -1,64 +1,118 @@
 import { NavLink } from 'react-router-dom';
 import './Register.css';
+import { FormEvent, useEffect, useRef, useState } from 'react';
+import FormInputGroup from '../FormInputGroup/FormInputGroup';
+
+const EMAIL_REGEX = /^\S+@\S+\.\S+$/;
 
 function Register()
 {
+    const usernameRef = useRef<HTMLInputElement>(null);
+    const emailRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+
+    const [username, SetUsername] = useState('');
+    const [usernameErr, SetUsernameErr] = useState(false);
+
+    const [email, SetEmail] = useState('');
+    const [emailErr, SetEmailErr] = useState(false);
+
+    const [password, SetPassword] = useState('');
+    const [confirmPassword, SetConfirmPassword] = useState('');
+    const [passwordErr, SetPasswordErr] = useState(false);
+
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleSubmit = (event : FormEvent<HTMLFormElement>) => 
+    {
+        event.preventDefault();
+
+        // username validation
+        const validUsername = username.length < 24 && username.length > 3;
+        SetUsernameErr(!validUsername);
+        
+        if (!validUsername)
+        {
+            usernameRef.current?.focus();
+            setErrorMessage('Username must be between 4 and 23 characters long');
+            return;
+        }
+
+        // email validation
+        const validEmail = EMAIL_REGEX.test(email);
+        SetEmailErr(!validEmail);
+        
+        if (!validEmail)
+        {
+            emailRef.current?.focus();
+            setErrorMessage('Invalid email');
+            return;
+        }
+
+        // password validation
+        const validPassword = password.length < 24 && password.length > 7;
+        SetPasswordErr(!validPassword);
+
+        if (!validPassword)
+        {
+            passwordRef.current?.focus();
+            setErrorMessage('Password must be between 8 and 24 characters long');
+            return;
+        }
+
+        if (password !== confirmPassword)
+        {
+            passwordRef.current?.focus();
+            SetPasswordErr(true);
+            setErrorMessage('Passwords do not match');
+            return;
+        }
+
+        setErrorMessage('');
+    };
+
+    useEffect(() => 
+    {
+        usernameRef.current?.focus();
+    }, []);
+
     return (
         <div className='register-content'>
             <div className='register-form'>
                 <div className='form-header'>
                     Register
                 </div>
-                <form className='form'>
+                <form className='form' onSubmit={handleSubmit}>
                     <div className='form-element'>
-                        <div className='error hidden'>
-                            Error
+                        <div className={(errorMessage ? 'error' : 'hidden')}>
+                            {errorMessage}
                         </div>
                     </div>
-                    <div className='form-element'>
-                        <label htmlFor='username' className='form-label'>
-                            Username *
-                        </label>
-                        <input 
-                            type='text'
-                            id='username'
-                            className='form-input'
-                            required
-                        />
-                    </div>
-                    <div className='form-element'>
-                        <label htmlFor='email' className='form-label'>
-                            Email address *
-                        </label>
-                        <input 
-                            type='text'
-                            id='email'
-                            className='form-input'
-                            required
-                        />
-                    </div>
-                    <div className='form-element'>
-                        <label htmlFor='password' className='form-label'>
-                            Password *
-                        </label>
-                        <input 
-                            type='password'
-                            id='password'
-                            className='form-input'
-                            required
-                        />
-                    </div>                   
-                    <div className='form-element'>
-                        <label htmlFor='confirmPassword' className='form-label'>
-                            Confirm Password *
-                        </label>
-                        <input 
-                            type='password'
-                            id='confirmPassword'
-                            className='form-input'
-                            required
-                        />
-                    </div>
+                    <FormInputGroup 
+                        description='Username *'
+                        hasError={usernameErr}
+                        inputRef={usernameRef} 
+                        onChange={SetUsername}
+                    />
+                    <FormInputGroup
+                        description='Email address *'
+                        hasError={emailErr}
+                        inputRef={emailRef}
+                        onChange={SetEmail}
+                    />
+                    <FormInputGroup
+                        description='Password *'
+                        hasError={passwordErr}
+                        inputRef={passwordRef}
+                        type='password'
+                        onChange={SetPassword}
+                    />
+                    <FormInputGroup
+                        description='Confirm password *'
+                        hasError={passwordErr}
+                        type='password'
+                        onChange={SetConfirmPassword}
+                    />
                     <div className='form-element'>
                         <button className='form-button'>
                             Register Account

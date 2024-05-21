ee10cd2f771913422a9a051e01ae16402e69fd29
Fabian Mahdi <fabian@mahdi.at>
2024-05-21 13:04:51 +0200

    added Login form + tests

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,6 +1,79 @@
+import { NavLink } from 'react-router-dom';
+import FormInputGroup from '../FormInputGroup/FormInputGroup';
+import './Login.css';
+import { FormEvent, useRef, useState } from 'react';
+
 function Login()
 {
-    return <h1>Login</h1>;
+    const usernameRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+
+    const [username, SetUsername] = useState('');
+    const [password, SetPassword] = useState('');
+
+    const [hasError, SetHasError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleSubmit = (event : FormEvent<HTMLFormElement>) => 
+    {
+        event.preventDefault();
+
+        const validInput = (username.length < 24 && username.length > 3) &&
+                           (password.length < 24 && password.length > 7);
+        SetHasError(!validInput);
+        
+        if (!validInput)
+        {
+            usernameRef.current?.focus();
+            setErrorMessage('Invalid username or password');
+        }
+        else
+        {
+            setErrorMessage('');
+        }
+    };
+
+    return (
+        <div className='login-content'>
+            <div className='login-form'>
+                <div className='form-header'>
+                    Login
+                </div>
+                <form className='form' onSubmit={handleSubmit}>
+                    <div className='form-element'>
+                        <p aria-label='error' className={(errorMessage ? 'error' : 'hidden')}>
+                            {errorMessage}
+                        </p>
+                    </div>
+                    <FormInputGroup 
+                        description='Username *'
+                        ariaLabel='username'
+                        hasError={hasError}
+                        inputRef={usernameRef} 
+                        onChange={SetUsername}
+                    />
+                    <FormInputGroup
+                        description='Password *'
+                        ariaLabel='password'
+                        type='password'
+                        hasError={hasError}
+                        inputRef={passwordRef}
+                        onChange={SetPassword}
+                    />
+                    <div className='form-element'>
+                        <button aria-label='submit' className='form-button'>
+                            Login
+                        </button>
+                    </div>
+                    <div className='form-element'>
+                        <NavLink aria-label='register' className='link register-link' to='/register'>
+                            Need an account? Register now.
+                        </NavLink>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default Login;
--- tests/Components/Login/Login.test.tsx
+++ tests/Components/Login/Login.test.tsx
@@ -1,15 +1,73 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Login from '../../../src/Components/Login/Login';
+import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 
 describe('Login' , () => 
 {
     const renderComponent = () => 
     {
-        render(<Login />);
+        render(
+            <MemoryRouter>
+                <Login />
+            </MemoryRouter>
+        );
+
+        return ({
+            error: screen.getByRole('paragraph', {name:/error/i}),
+            usernameInput: screen.getByRole('textbox', {name:/username/i}),
+            passwordInput: screen.getByLabelText(/password/i),
+            submit: screen.getByRole('button', {name:/submit/i}),
+            user: userEvent.setup()
+        });
     };
 
-    it('should render all elements without failure', () => 
+    test.each([
+        ['validUsername', 'p'],
+        ['validUsername', 'veryveryveryveryveryveryveryveryverylongPassword'],
+        ['u', 'validPassword'],
+        ['veryveryveryveryveryveryveryveryverylongUsername', 'validPassword'],
+    ])('should display an error when form is submitted with invalid values', async (
+        username: string,
+        password: string
+    ) => 
     {
-        renderComponent();
+        const {
+            error,
+            usernameInput,
+            passwordInput,
+            submit,
+            user
+        } = renderComponent();
+
+        await user.type(usernameInput, username);
+        await user.type(passwordInput, password);
+
+        await user.click(submit);
+
+        expect(error.textContent).is.not.empty;
+    });
+
+    test.each([
+        ['validUsername', 'validPassword'],
+    ])('should not display an error when form is submitted with valid values', async (
+        username: string,
+        password: string
+    ) => 
+    {
+        const {
+            error,
+            usernameInput,
+            passwordInput,
+            submit,
+            user
+        } = renderComponent();
+
+        await user.type(usernameInput, username);
+        await user.type(passwordInput, password);
+
+        await user.click(submit);
+
+        expect(error.textContent).is.empty;
     });
 });
